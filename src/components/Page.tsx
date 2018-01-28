8821ffd0e00e279799a2428b089abcc610fd9bdb
Alf Eaton <aeaton@atypon.com>
2018-01-28 08:49:42 +0000

    Start using variables from the theme

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -4,10 +4,14 @@ export const Page = styled('div')`
   display: flex;
   min-height: 100vh;
   box-sizing: border-box;
+  background-color: ${props => props.theme.backgroundColor};
+  color: ${props => props.theme.color};
+  font-family: ${props => props.theme.fontFamily};
 `
 
 export const Sidebar = styled('div')`
   width: 200px;
+  padding: ${props => props.theme.padding};
   border-right: 1px solid #aaa;
 `
 
--- src/theme.tsx
+++ src/theme.tsx
@@ -2,8 +2,14 @@ import * as React from 'react'
 import { ThemeProvider as Provider } from 'styled-components'
 import { ChildrenProps } from './types'
 
+// tslint:disable:max-line-length
+
 const theme = {
-  main: 'green',
+  backgroundColor: '#3f566f',
+  color: '#fff',
+  fontFamily: `'Barlow', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+  'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+  padding: '10px',
 }
 
 export const ThemeProvider = (props: ChildrenProps) => (
