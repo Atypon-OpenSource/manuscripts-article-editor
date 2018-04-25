340147a04360a9d49dfacbf47be780ffa2e4ec58
Alf Eaton <eaton.alf@gmail.com>
2018-04-25 20:19:54 +0100

    Set user data to null when signing out

--- src/store/UserProvider.tsx
+++ src/store/UserProvider.tsx
@@ -159,6 +159,7 @@ class UserProvider extends React.Component<ComponentsProps> {
           this.setState({
             loading: false,
             loaded: true,
+            data: null,
           })
         } else {
           // any other error
