4737b14cb1fb49f32b1e55d7872911b8fa9cc802
Alf Eaton <eaton.alf@gmail.com>
2018-05-31 16:59:02 +0100

    Add Developer menu

---
+++ src/containers/DeveloperMenu.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { createToken, createUserProfile } from '../lib/developer'
+import { removeDB } from '../lib/rxdb'
+import { getCurrentUserId } from '../store/UserProvider'
+import { styled } from '../theme'
+
+const DropdownAction = styled.div`
+  padding: 10px 20px;
+  display: block;
+  white-space: nowrap;
+  cursor: pointer;
+
+  &:hover {
+    background: #7fb5d5;
+    color: white;
+  }
+`
+
+export const MenuLabel = styled.div`
+  display: inline-flex;
+  align-items: center;
+  padding: 5px 10px;
+  text-decoration: none;
+  color: inherit;
+  border-radius: 4px;
+  margin-left: 20px;
+`
+
+export const DeveloperMenu = () => (
+  <React.Fragment>
+    <DropdownAction
+      onClick={async () => {
+        await removeDB()
+        alert('Removed database')
+        window.location.reload(true)
+      }}
+    >
+      Delete database
+    </DropdownAction>
+
+    <DropdownAction
+      onClick={() => {
+        createToken('demo@example.com')
+        alert('Created token')
+      }}
+    >
+      Create token
+    </DropdownAction>
+
+    <DropdownAction
+      onClick={async () => {
+        const userId = getCurrentUserId()
+
+        await createUserProfile(userId as string, {
+          given: 'Example',
+          family: 'User',
+        })
+
+        alert('Created user profile')
+      }}
+    >
+      Create user profile
+    </DropdownAction>
+  </React.Fragment>
+)
---
+++ src/lib/developer.ts
@@ -0,0 +1,28 @@
+import { USER_PROFILE } from '../transformer/object-types'
+import { BibliographicName } from '../types/components'
+import { Db, waitForDB } from './rxdb'
+import token from './token'
+
+export const createToken = (userId: string) => {
+  token.set({
+    access_token: ['', btoa(JSON.stringify({ userId })), ''].join('.'),
+  })
+}
+
+export const createUserProfile = async (
+  userId: string,
+  bibliographicName: Partial<BibliographicName>
+) => {
+  if (!userId) {
+    alert('Create a token first')
+  }
+
+  const db: Db = await waitForDB
+
+  await db.components.upsert({
+    id: `${USER_PROFILE}:${userId.replace('_', '|')}`,
+    objectType: USER_PROFILE,
+    user_id: userId,
+    bibliographicName,
+  })
+}
