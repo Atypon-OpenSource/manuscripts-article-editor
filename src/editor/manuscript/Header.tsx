ba3e09c2f5fc08a38f26804caed5e546bdc7b3f8
Alf Eaton <eaton.alf@gmail.com>
2018-06-20 16:31:35 +0100

    Fix header padding

--- src/editor/manuscript/Header.tsx
+++ src/editor/manuscript/Header.tsx
@@ -2,6 +2,6 @@ import { styled } from '../../theme'
 
 export const Header = styled.header`
   background-color: #ecf0f4;
-  padding: 20px 50px;
-  margin: 0 -50px 20px;
+  padding: 20px 40px;
+  margin: 0 -40px 20px;
 `
