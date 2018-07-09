98f4e9f94677f694add076294c32bec1a6fde2fa
Matias Piipari <matias.piipari@gmail.com>
2018-07-09 21:51:53 +0000

    Fixes some silly conflict markers...

--- docker/tests/testcafe/wait-for-client.sh
+++ docker/tests/testcafe/wait-for-client.sh
@@ -21,24 +21,14 @@ attempts=0
 until $(curl -o /dev/null -s --head --fail "${API_BASE_URL}/app/version")
 do
   attempts=$((attempts + 1))
-<<<<<<< HEAD
-  if [ $attempts -eq 512 ]
-  then
-    echo "API did not appear."
-=======
   if [ $attempts -eq 256 ]
   then
-    echo "Client did not appear."
->>>>>>> eb564235b47e0d4b57941eadc1d41e7f6e2f7c3a
+    echo "API did not appear."
     exit 1
   fi
   sleep 1
 done
 
-<<<<<<< HEAD
-echo "Connected to API."
-=======
 echo "Connected to API…"
->>>>>>> eb564235b47e0d4b57941eadc1d41e7f6e2f7c3a
 
 exec "$@"
