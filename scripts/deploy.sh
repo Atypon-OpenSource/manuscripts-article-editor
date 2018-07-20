6d01c85c30296d40921e0db74c17496349ef158f
Alf Eaton <eaton.alf@gmail.com>
2018-07-20 09:48:30 +0100

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,13 +10,11 @@ aws s3 sync --exclude "index.html" --exclude "service-worker.js" \
     --cache-control "max-age=2592000" --acl "public-read" \
      dist/ s3://${S3_BUCKET}
 
-aws s3 cp --include "index.html" \
-    --cache-control "max-age=0" --acl "public-read" \
-     dist/ s3://${S3_BUCKET}
+aws s3 cp "dist/index.html" s3://${S3_BUCKET}/index.html \
+    --cache-control "max-age=0" --acl "public-read"
 
- aws s3 cp --include "service-worker.js" \
-    --cache-control "no-cache" --acl "public-read" \
-     dist/ s3://${S3_BUCKET}
+ aws s3 cp "dist/service-worker.js" s3://${S3_BUCKET}/service-worker.js \
+    --cache-control "no-cache" --acl "public-read"
 
 aws cloudfront create-invalidation \
     --distribution-id ${CLOUDFRONT_ID} --paths /index.html /
