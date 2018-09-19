6e3073f8d34b40685f9e76dfb58b4cbf17484bf5
Alf Eaton <eaton.alf@gmail.com>
2018-09-19 22:41:18 +0100

    Remove publish_image job

--- scripts/publish-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e # exit if any step fails
-set -u # exit if a variable isn't set
-
-docker login -u gitlab-ci-token -p ${CI_JOB_TOKEN} registry.gitlab.com
-docker pull ${CONTAINER_IMAGE} || true
-docker build -f docker/client/production/Dockerfile --cache-from ${CONTAINER_IMAGE} --tag ${CONTAINER_IMAGE} .
-docker push ${CONTAINER_IMAGE}
