cd1b2a77f916c1c660de5f2c739f6e43e6351e93
Kaisa Piipari <kaisa.piipari@gmail.com>
2018-11-05 22:25:28 +0000

    Adds a figure-captions Cypress test that tests for inserting a figure, inserting a figure caption, exiting a figure caption and deleting a figure panel.

---
+++ cypress/integration/figure-captions.js
@@ -0,0 +1,45 @@
+import { generateUser, login, signup } from './helpers.spec'
+
+describe('Figures', () => {
+    const user = generateUser()
+    
+    before(() => {
+        signup(user, true)
+    })
+
+    it('Can insert a figure block caption', () => {
+        login(user, true)
+
+        cy.get('#create-project').click()
+        cy.get('.manuscript-editor').type('{enter}')
+        cy.get('[data-cy="Insert figure"]').click()
+        cy.get('.figure-panel').should('exist')
+        cy.get('[tabindex="2"]').type('Metamotif estimation from simulated motif data.')
+        cy.get('figcaption').should('have.text', 'Figure 1:Metamotif estimation from simulated motif data.')
+    })
+
+    it('Can exit a figure caption with enter', () => {
+        login(user, true)
+
+        cy.get('#create-project').click()
+        cy.get('.manuscript-editor').type('{enter}')
+        cy.get('[data-cy="Insert figure"]').click()
+        cy.get('[tabindex="2"]').type('Test caption. {enter}')
+        cy.get('p').last()
+        cy.get('[tabindex="2"]').type('hi')
+        cy.get('p').last().should('have.text', 'hi')
+    })
+
+    it('Can delete a figure panel', () => {
+        login(user, true)
+
+        cy.get('#create-project').click()
+        cy.get('.manuscript-editor').type('{enter}')
+        cy.get('[data-cy="Insert figure"]').click()
+        cy.get('.block-figure_element > .block-gutter > .edit-block > svg').click()
+        cy.get(':nth-child(3) > .menu-item').click()
+        cy.get('.figure-panel').should('not.exist')
+    })
+
+    
+})
