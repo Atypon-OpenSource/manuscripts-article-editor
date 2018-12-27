64c5cea3f3b6c358f963df936e6e65726ade09bd
Bader Al-Hamdan <bhamdan@atypon.com>
2018-12-27 09:44:55 +0200

    add more stories for alert messages and buttons

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,13 +2,35 @@ import { action } from '@storybook/addon-actions'
 import { withNotes } from '@storybook/addon-notes'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
-import { Button, IconButton, PrimaryButton } from '../src/components/Button'
+import {
+  Button,
+  IconButton,
+  ManuscriptBlueButton,
+  PrimaryButton,
+  TransparentBlackButton,
+  TransparentGreyButton,
+} from '../src/components/Button'
 import GoogleIcon from '../src/icons/google'
 
 storiesOf('Buttons', module)
   .add('Button', () => <Button onClick={action('clicked')}>Make</Button>)
   .add('Primary Button', () => (
     <PrimaryButton onClick={action('clicked')}>Done</PrimaryButton>
+  ))
+  .add('Manuscripts Blue Button', () => (
+    <ManuscriptBlueButton onClick={action('clicked')}>
+      Save
+    </ManuscriptBlueButton>
+  ))
+  .add('Transperent Grey Button', () => (
+    <TransparentGreyButton onClick={action('clicked')}>
+      Cancel
+    </TransparentGreyButton>
+  ))
+  .add('Transperent Black Button', () => (
+    <TransparentBlackButton onClick={action('clicked')}>
+      Cancel
+    </TransparentBlackButton>
   ))
   .add(
     'Icon Button',
