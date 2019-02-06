6275e81be0d7abf580736ac988aff5d4b4ac295e
Alf Eaton <eaton.alf@gmail.com>
2019-02-06 16:22:12 +0000

    Revert to pouchdb-adapter-memory for mock db

--- src/lib/__mocks__/adapter.ts
+++ src/lib/__mocks__/adapter.ts
@@ -1,7 +1,6 @@
-import memdown from 'memdown'
-import PouchDBLevelAdapter from 'pouchdb-adapter-leveldb'
+import PouchDBLevelAdapter from 'pouchdb-adapter-memory'
 import RxDB from '../rxdb'
 
 RxDB.plugin(PouchDBLevelAdapter)
 
-export const adapter = memdown
+export const adapter = 'memory'
