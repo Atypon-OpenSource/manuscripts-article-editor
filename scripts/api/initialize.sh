d9aa7161e2a3d53be955b4acd5b230ee35ca7666
Alf Eaton <aeaton@atypon.com>
2020-06-30 20:57:55 +0100

    Use api-initialize container in initialize scripts

--- scripts/api/initialize.sh
+++ scripts/api/initialize.sh
@@ -3,11 +3,10 @@
 set -e
 
 export COUCHBASE_INITIALIZE=true
-export APP_INITIALIZE=1
-export APP_RUN_AFTER_INITIALIZE=0
 export PYTHONUNBUFFERED=1
 
 docker-compose down --volumes
 docker-compose up -d couchbase
 docker-compose up -d sync-gateway
-docker-compose up api
+docker-compose up api-initialize
+docker-compose down
