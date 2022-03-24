4babb1d3d1371c0db9decd126ff9774abf3d2b08
mbartenev-atypon <mbartenev@atypon.com>
2022-03-24 17:11:03 +0100

    Islam Momani: Add suggestion when figure updated -LEAN-739

--- src/hooks/use-commits.ts
+++ src/hooks/use-commits.ts
@@ -20,6 +20,7 @@ import {
   checkout,
   commands,
   Commit,
+  commitToJSON,
   findCommitWithChanges,
   findCommitWithin,
   getChangeSummary,
@@ -117,12 +118,20 @@ export const useCommits = ({ sortBy, editor }: Args) => {
       return
     }
     const changeSummary = getChangeSummary(editor.state, commit.changeID)
-    if (
-      !changeSummary ||
-      (changeSummary.deletion.length === 0 &&
-        changeSummary.insertion.length === 0)
-    ) {
+
+    if (!changeSummary) {
       return
+    }
+
+    let changeType = 'text'
+    //This is needed because some changes have no inserted or deleted text, but we still need to define the change type (i.e figure update), so we can add a suggestion indicating that change.
+
+    if (
+      changeSummary.deletion.length === 0 &&
+      changeSummary.insertion.length === 0
+    ) {
+      changeType = commitToJSON(commit, store.projectID).steps[0].slice
+        .content[0].type
     }
 
     setIsDirty(true)
@@ -136,8 +145,11 @@ export const useCommits = ({ sortBy, editor }: Args) => {
       containerID: store.projectID,
       manuscriptID: store.manuscriptID,
       snapshotID: store.snapshotID || '',
-      insertion: changeSummary ? changeSummary.insertion : '',
-      deletion: changeSummary ? changeSummary.deletion : '',
+      insertion:
+        changeSummary.insertion ||
+        (changeType == 'figure' && 'IMAGE UPDATED') ||
+        '',
+      deletion: changeSummary.deletion || '',
       positionInSnapshot: changeSummary ? changeSummary.ancestorPos : undefined,
       status: {
         label: asAccepted ? 'accepted' : 'proposed',
