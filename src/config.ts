bdbb210444d54fd6e2cc6b01844503646046d886
asouqi <asouqi@atypon.com>
2022-10-18 12:57:46 +0300

    Update Dashboard Configurations (LEAN-1810)

--- src/config.ts
+++ src/config.ts
@@ -53,6 +53,7 @@ interface Config {
     DOI: boolean
     runningTitle: boolean
     figureAlignment: boolean
+    projectMenu: boolean
     requirements: boolean
   }
   gateway: {
@@ -182,6 +183,7 @@ const config = {
     DOI: isTrue(process.env.FEATURE_DOI),
     runningTitle: isTrue(process.env.FEATURE_RUNNING_TITLE),
     figureAlignment: isTrue(process.env.FEATURE_FIGURE_ALIGNMENT),
+    projectMenu: isTrue(process.env.FEATURE_PROJECT_MENU),
     requirements: isTrue(process.env.REQUIREMENTS_ENABLED),
   },
   gateway: {
