0f3b15f9c2acf4932d036f4f60bf0fa6b9e5104f
mbartenev-atypon <mbartenev@atypon.com>
2022-12-11 22:53:49 +0100

    updates

--- src/postgres-data/PsSource.ts
+++ src/postgres-data/PsSource.ts
@@ -10,6 +10,7 @@
  * All portions of the code written by Atypon Systems LLC are Copyright (c) 2019 Atypon Systems LLC. All Rights Reserved.
  */
 import { FileManagement } from '@manuscripts/style-guide'
+
 import { builderFn, state, stateSetter } from '../store'
 import { StoreDataSourceStrategy } from '../store/DataSourceStrategy'
 import Api from './Api'
