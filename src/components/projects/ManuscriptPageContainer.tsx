8a8432884b549cdaf1ce9190b8d390f9a75b95e1
Noura Ghazzawi <nghazzawi@atypon.com>
2023-11-18 20:41:50 +0300

    fix building issues

--- src/components/projects/ManuscriptPageContainer.tsx
+++ src/components/projects/ManuscriptPageContainer.tsx
@@ -193,6 +193,7 @@ const ManuscriptPageView: React.FC = () => {
         view={view}
         state={state}
         user={user}
+        doc ={doc}
       />
 
       <PageWrapper onClick={onAppClick}>
