209404a74c516d85965cf3d3e456324d77dc958f
mbartenev-atypon <mbartenev@atypon.com>
2024-03-11 18:14:26 +0100

    cleanup

--- src/hooks/use-create-editor.ts
+++ src/hooks/use-create-editor.ts
@@ -12,7 +12,6 @@
 import { useEditor } from '@manuscripts/body-editor'
 import { CommentAnnotation } from '@manuscripts/json-schema'
 import { getCapabilities as getActionCapabilities } from '@manuscripts/style-guide'
-import { trackChangesPlugin } from '@manuscripts/track-changes-plugin'
 import { buildContribution } from '@manuscripts/transform'
 import { memoize } from 'lodash'
 import { useHistory } from 'react-router'
@@ -81,20 +80,13 @@ export const useCreateEditor = () => {
       tabindex: '2',
     },
     doc,
-    plugins: config.quarterback.enabled
-      ? [
-          trackChangesPlugin({
-            userID: user._id,
-            debug: config.environment === 'development',
-          }),
-        ]
-      : [],
+    userID: user._id,
+    debug: config.environment === 'development',
     locale: manuscript?.primaryLanguageCode || 'en-GB',
     cslProps: {
       style,
       locale,
     },
-    environment: config.environment,
     history,
     popper,
     projectID: project._id,
