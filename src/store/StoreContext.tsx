159d33eb5c92560ab9eb91afffbe1db618f5d3b3
mnatsheh <mnatsheh@atypon.com>
2024-07-11 08:15:33 -0700

    Small fix

--- src/store/StoreContext.tsx
+++ src/store/StoreContext.tsx
@@ -11,18 +11,15 @@
  */
 import React, { createContext, useContext, useEffect } from 'react'
 
-import { AppStateObserver } from '../EditorApp'
 import { GenericStore, StoreDataSourceStrategy } from '.'
 
 const GenericStoreContext = createContext<GenericStore>(new GenericStore())
 
 export const createStore = async (
   sources: StoreDataSourceStrategy[],
-  observer?: AppStateObserver
 ) => {
   const store = new GenericStore()
   await store.init(sources)
-  store.subscribe((state) => observer?.onUpdate(state))
   return store
 }
 
