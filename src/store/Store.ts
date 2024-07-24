aae6625f3d3c940fb68d22815daa04ee166e4409
mbartenev-atypon <mbartenev@atypon.com>
2024-07-24 18:42:32 +0200

    removing stuff

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -63,12 +63,6 @@ export type state = {
   deleteModel: (id: string) => Promise<string>
   saveModels: (models: Model[]) => Promise<void>
   saveManuscript: (data: Partial<Manuscript>) => Promise<void>
-  // track changes doc state changes
-  saveTrackModel: <T extends Model>(
-    model: T | Build<T> | Partial<T>
-  ) => Promise<T>
-  trackModelMap: Map<string, Model>
-  deleteTrackModel: (id: string) => Promise<string>
 
   fileManagement: FileManagement
   files: FileAttachment[]
