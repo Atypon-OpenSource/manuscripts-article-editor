aaa60a6e8e004f064bc94b88ce88f4587f5bcfb9
Fahid Alwaisi <82392169+fwaisi@users.noreply.github.com>
2025-01-26 16:10:51 +0300

    Update TrackModal.tsx

--- src/components/track-changes/TrackModal.tsx
+++ src/components/track-changes/TrackModal.tsx
@@ -199,7 +199,7 @@ export const TrackModal = forwardRef<PropRef, Props>((props, ref) => {
                     <OldAttribute>{oldAttrs[key].value}</OldAttribute>
                   </div>
                 ) : null}
-                {newAttrs[key]?.value.length > 0 && (
+                {newAttrs[key]?.value && newAttrs[key]?.value.length > 0 && (
                   <div>
                     <InsertLabel>Inserted: </InsertLabel>
                     <AttributeValue>{newAttrs[key].value}</AttributeValue>
