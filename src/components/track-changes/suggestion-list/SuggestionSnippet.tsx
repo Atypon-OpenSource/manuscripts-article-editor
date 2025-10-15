a9d2a6fa826e8d2e1033eb4e87ccb8528e94a71f
Marek V. <mvidtman@atypon.com>
2025-10-15 15:26:16 +0200

    LEAN-4203: color fix

--- src/components/track-changes/suggestion-list/SuggestionSnippet.tsx
+++ src/components/track-changes/suggestion-list/SuggestionSnippet.tsx
@@ -82,10 +82,13 @@ export const SuggestionSnippet: React.FC<Props> = ({
               <AvatarIcon width={16} height={16} />
               <AuthorName>{authorName}</AuthorName>{' '}
             </AuthorContainer>
-            <MetadataDate date={dataTracked?.createdAt} />
+            <MetadataDate
+              date={dataTracked?.createdAt}
+              isTrackingChangesVisible={isTrackingChangesVisible}
+            />
           </CardMetadata>
           {!isComparingMode && (
-            <CardActions>
+            <CardActions data-cy="card-actions">
               <SuggestionActions
                 suggestions={suggestions}
                 handleAccept={handleAccept}
@@ -97,9 +100,7 @@ export const SuggestionSnippet: React.FC<Props> = ({
         <CardBody data-cy="card-body">
           {snippet?.operation && (
             <Operation
-              color={
-                isTrackingChangesVisible ? dataTracked.operation : 'default'
-              }
+              color={isTrackingChangesVisible ? dataTracked.operation : 'muted'}
             >
               {snippet?.operation}:
             </Operation>
@@ -125,6 +126,10 @@ export const Card = styled.div<{
   isFocused: boolean
   isTrackingChangesVisible: boolean
 }>`
+  font-family: ${(props) => props.theme.font.family.sans};
+  font-size: ${(props) => props.theme.font.size.small};
+  line-height: ${(props) => props.theme.font.lineHeight.normal};
+
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
@@ -135,7 +140,9 @@ export const Card = styled.div<{
   border: ${(props) =>
     isActive(props)
       ? `1px solid ${props.theme.colors.border.tracked.active}`
-      : `1px solid ${props.theme.colors.border.tracked.default}`};
+      : props.isTrackingChangesVisible
+      ? `1px solid ${props.theme.colors.border.tracked.default}`
+      : `1px solid #dfdfdf`};
   box-shadow: ${(props) =>
     isActive(props)
       ? `-4px 0 0 0  ${props.theme.colors.border.tracked.active}`
@@ -144,7 +151,10 @@ export const Card = styled.div<{
 
   /* FocusHandle should cover entire card: */
   position: relative;
-  color: ${(props) => props.theme.colors.text.primary};
+  color: ${(props) =>
+    props.isTrackingChangesVisible
+      ? props.theme.colors.text.primary
+      : '#868686'};
   background: ${(props) =>
     isActive(props)
       ? props.theme.colors.background.tracked.active + ' !important'
@@ -197,30 +207,29 @@ export const AuthorContainer = styled.div`
   gap: 8px;
 `
 export const AuthorName = styled.span`
-  font-family: Lato, sans-serif;
-  font-size: 14px;
+  font-size: ${(props) => props.theme.font.size.normal};
   font-weight: 700;
   line-height: 24px;
 `
-const MetadataDate = styled(RelativeDate)`
-  color: ${(props) => props.theme.colors.text.secondary};
+const MetadataDate = styled(RelativeDate)<{
+  isTrackingChangesVisible: boolean
+}>`
+  color: ${(props) =>
+    props.isTrackingChangesVisible
+      ? props.theme.colors.text.secondary
+      : '#868686'};
 `
 
 const SnippetText = styled.div`
-  font-size: ${(props) => props.theme.font.size.small};
   line-height: ${(props) => props.theme.font.lineHeight.normal};
   width: 100%;
   white-space: nowrap;
-  color: ${(props) => props.theme.colors.text.primary};
   display: block;
 `
 
 const Operation = styled.span<{ color: string }>`
-  font-family: Lato, sans-serif;
-  font-size: 12px;
-  font-weight: 700;
+  font-weight: ${(props) => props.theme.font.weight.bold};
   text-transform: capitalize;
-  line-height: 16px;
   margin-right: 3.2px;
   color: ${(props) => {
     switch (props.color) {
@@ -234,6 +243,8 @@ const Operation = styled.span<{ color: string }>`
       case 'move':
       case 'set_attrs':
         return '#0284B0'
+      case 'muted':
+        return '#868686'
       default:
         return '#353535'
     }
@@ -242,9 +253,6 @@ const Operation = styled.span<{ color: string }>`
 
 const NodeName = styled.span`
   text-transform: capitalize;
-  color: #353535;
-  font-family: Lato, sans-serif;
-  font-size: 12px;
-  font-weight: bold;
-  line-height: 16px;
+  font-weight: ${(props) => props.theme.font.weight.bold};
+  // line-height: 16px;
 `
